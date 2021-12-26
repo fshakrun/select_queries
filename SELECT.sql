c46886cd91e72c95634c7184786238f7fc9f5407
Fyodor Sh <82056292+fshakrun@users.noreply.github.com>
2021-12-26 16:21:32 +0300

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -1,7 +1,7 @@
-SELECT title, release_date FROM Album 
+SELECT album_title, release_date FROM Album 
 WHERE release_date = 2018;
 
-SELECT title, track_duration FROM Tracks 
+SELECT track_title, track_duration FROM Tracks 
 ORDER BY track_duration desc 
 LIMIT 1;
 
@@ -11,8 +11,8 @@ WHERE track_duration >= 3.5;
 SELECT mt_title FROM Mix_tapes
 WHERE release_date BETWEEN 2018 AND 2020;
 
-SELECT Artist_name FROM Artist_list 
-WHERE Artist_name not like '% %';
+SELECT artist_name FROM Artist_list 
+WHERE artist_name not like '% %';
 
 SELECT track_title FROM Tracks 
 WHERE (track_title LIKE '%мой%') OR (track_title LIKE '%My%') OR (track_title LIKE '%Мой%');
